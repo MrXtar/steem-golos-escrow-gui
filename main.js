088fd55d313489253b2a7d6eb8b1a86ea9f84723
Xtar <SD78fheif>
2017-02-16 12:34:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,359 @@
+var transaction;
+
+Date.prototype.toYMD = Date_toYMD;
+function Date_toYMD() {
+	var year = String(this.getFullYear()),
+	month = ('00' + (this.getUTCMonth()+1)).slice(-2), 
+	day = ('00' + this.getUTCDate()).slice(-2), 
+	hours = ('00' + this.getUTCHours()).slice(-2), 
+	minutes = ('00' + this.getUTCMinutes()).slice(-2), 
+	seconds = ('00' + this.getUTCSeconds()).slice(-2);
+	return year + "-" + month + "-" + day + 'T' + hours + ':' + minutes + ':' + seconds;
+}
+
+function passToWif(login, pass) {
+	if(pass.charAt(0) == '5' && pass.length == 51) {
+		return pass;
+	} else {
+		return steem.auth.toWif(login, pass, 'active');
+	}
+}
+
+function gup( name ) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+	return "";
+  else
+	return results[1];
+}		
+
+function Transaction(id, onLoadCallback) {
+	if(!id.match( /^[a-z0-9\.\-]+-[\d]+$/ )) {
+		alert('Не верный ID ' + id);
+		return false;
+	}
+	
+	var parts = id.split('-'),
+		obj = this;
+	obj.from = parts[0];
+	obj.escrow_id = parts[1];	
+	
+	steem.api.getEscrow(
+		obj.from,
+		obj.escrow_id,
+		function(err, response) {
+			if(!err && !response) {
+				$('#result div.alert-danger').slideDown();
+			} else if (typeof response == 'object') {
+				console.log(response);
+				$.each(response, function(index, value) {
+					obj[index] = value;
+				}); 
+				if(obj.steem_balance == '0.000 GOLOS') {
+					obj.money = obj.sbd_balance;
+				} else {
+					obj.money = obj.steem_balance;
+				}
+				
+				
+				if(!obj.agent_approved || !obj.to_approved) {
+					obj.status = 'waitingForApproval';
+				} else if (obj.disputed) {
+					obj.status = 'disputeInitiated';
+				} else {
+					obj.status = 'approvalRecieved';
+				}
+				
+				onLoadCallback(obj);
+			}
+			console.log(err, obj);
+		}
+	);
+
+	obj.submitApprove = function(login, pass, approve, callback) {		
+		steem.broadcast.escrowApprove(
+			passToWif(login, pass),
+			obj.from,
+			obj.to,
+			obj.agent,
+			login,
+			obj.escrow_id,
+			approve,
+			function(err, response) {
+				console.log(err, response);
+				callback();
+			}
+		);
+	}
+	
+	obj.submitRelease = function(login, pass, callback) {
+			steem.broadcast.escrowRelease(
+				passToWif(login, pass),
+				obj.from,
+				obj.to,
+				obj.agent,
+				login,			
+				login == obj.from ? obj.to : obj.from,
+				obj.escrow_id,
+				obj.sbd_balance,
+				obj.steem_balance,
+				function(err, response) {
+					console.log(err, response);
+					callback();
+				}
+			);
+	}
+	
+	obj.submitDispute = function(login, pass, callback) {
+		steem.broadcast.escrowDispute(
+			passToWif(login, pass),
+			obj.from,
+			obj.to,
+			obj.agent,
+			login,			
+			obj.escrow_id,
+			function(err, response) {
+				console.log(err, response);
+				callback();
+			}
+		);
+	}
+	
+	obj.submitEscrow = function(login, pass, reciever, callback) {
+		steem.broadcast.escrowRelease(
+			passToWif(login, pass),
+			obj.from,
+			obj.to,
+			obj.agent,
+			login,			
+			obj[reciever],
+			obj.escrow_id,
+			obj.sbd_balance,
+			obj.steem_balance,
+			function(err, response) {
+				console.log(err, response);
+				callback();
+			}
+		);
+	}	
+	
+}
+	
+$(function() {
+	$('a.inputToggle').click(function() {
+		var toggles = $(this).data('toggles').split('-');
+		if($(this).text() == toggles[0]) {
+			$('.inputToggle').text(toggles[1]);
+		} else {
+			$('.inputToggle').text(toggles[0]);
+		}
+		return false;
+	});
+
+	$('a#tabCP').click(function() {
+		if(!id) {
+			id = prompt('Введите ID транзакции. Например xtar-8724723:');									
+			if(!id) {
+				return false;
+			}
+			transaction = new Transaction(id);
+			if(transaction.from === undefined) {
+				id = null;
+				return false;
+			}
+		}
+		window.history.pushState('',id, '?id=' + id);
+		$(this).parent().addClass('active').siblings().removeClass('active');
+		$('#step1, #step2').hide();				
+		$('#controlPanel').show();
+		return false;
+	});
+
+	$('a#tabSend').click(function() {
+		$(this).parent().addClass('active').siblings().removeClass('active');
+		$('#step1').show();				
+		$('#controlPanel').hide();
+		return false;
+	});
+
+	$('#sendAgent').change(function() {
+		var agent = $('#sendAgent').val();
+		$('#sendFeeAmoutLabel').html('Комиссия от <a href="https://golos.io/@' + agent + '" target="_blank">@' + agent + '</a>');
+		$('#sendFeeAmout').val($('#sendAgent option:selected').data('fee'));
+	});
+	
+	$('#sendAgent').change();
+	
+	$('#sendSubmit').click(function() {
+		var btn = $(this),
+		from = $('#sendLogin').val(),
+		to = $('#sendReceiver').val(),
+		wif = passToWif(from, $('#sendPassword').val()),
+		agent = $('#sendAgent').val(),
+		escrow_id = parseInt(Math.random() * (99999999 - 10000000) + 10000000),
+		fee = parseFloat($('#sendAgent option:selected').data('fee')).toFixed(3) + ' ' + $('#sendAmountUnit').text(),
+		sbd_amount = '0.000 GBG',
+		steem_amount = '0.000 GOLOS',
+		meta = {
+			meta: $('#sendMeta').val()
+		};
+		
+		btn.prop('disabled', true);
+		
+		if($('#sendAmountUnit').text() == 'GOLOS') {
+			steem_amount = parseFloat($('#sendAmount').val().replace(',','.')).toFixed(3) + ' GOLOS';
+		} else if ($('#sendAmountUnit').text() == 'GBG') {
+			sbd_amount = parseFloat($('#sendAmount').val().replace(',','.')).toFixed(3) + ' GBG';
+		}
+
+		steem.api.getDynamicGlobalProperties(function(err, response) {
+			var ratification_deadline = new Date(response.time);
+			ratification_deadline.setMinutes(ratification_deadline.getMinutes() + 24 * 60 - 1);
+						
+			var escrow_expiration = new Date(response.time);
+			escrow_expiration.setHours(escrow_expiration.getHours() + parseInt($('#sendEscrowExpiration').val()));			
+			
+			//console.log(response.time, wif, from, to, agent, escrow_id, sbd_amount, steem_amount, fee, ratification_deadline.toYMD(), escrow_expiration.toYMD());
+			steem.broadcast.escrowTransfer(
+				wif, // active key
+				from,
+				to, // to
+				agent, // escrow nick
+				escrow_id,
+				sbd_amount, // amount gbg
+				steem_amount, // amount golos
+				fee, // fee
+				ratification_deadline.toYMD(),
+				escrow_expiration.toYMD(),
+				JSON.stringify(meta),
+				function(err, response) {
+					if(!err) {		
+						if(from) {
+							localStorage.setItem('escrow_login', from);
+						}
+						$('#step1').slideUp();
+						$('#step2').slideDown();
+						$('.sentLink').html('<a href="?id=' + from + '-' + escrow_id + '">https://golosim.ru/escrow/?id=' + from + '-' + escrow_id + '</a>');
+						$('.sentId').html(from + '-' + escrow_id);
+					} else {
+						$('#sendError').html('<br><b>Возникла ошибка:</b><br><br>' + err.payload.error.message.replace(/([^>])\n/g, '$1<br><br>'));
+						console.log(err, response);
+						btn.prop('disabled', false);
+					}
+				}
+			);
+		});
+	});
+	$('#escrowData').on('click', 'button', function() {	
+		$('#transactionType').val($(this).data('form'));	
+		$('form').hide();
+		$('#' + $(this).data('form')).show();		
+		$('#sendTransaction').hide();
+		$('#sendTransaction').slideDown();	
+		$('#login').val(transaction[$(this).data('nick')]);
+		$('#password').focus();
+		if($(this).data('answer') == '1') {
+			$('#approveYes').prop('checked', true);
+		} else if($(this).data('answer') == '0') {
+			$('#approveNo').prop('checked', true);
+		}
+		if($(this).data('answer') == 'escrowFrom') {
+			$('#escrowFrom').prop('checked', true);
+		} else if($(this).data('answer') == 'escrowTo') {
+			$('#escrowTo').prop('checked', true);
+		}
+	});
+	
+	$('#approveForm').submit(function(e) {
+		$(this).find('input[type=submit]').prop('disabled', true);
+		transaction.submitApprove(
+			$('#login').val(),
+			$('#password').val(),
+			$('#approveYes').is(':checked') ? 'true' : 'false',
+			function(err, response) {
+				location.reload();
+			}
+		);
+		return false;
+	});
+
+	$('#releaseForm, #cancelForm').submit(function(e) {			
+		$(this).find('input[type=submit]').prop('disabled', true);
+		e.preventDefault();
+		transaction.submitRelease(
+			$('#login').val(),
+			$('#password').val(),
+			function(err, response) {
+				location.reload();
+			}
+		);
+		return false;
+	});
+
+	$('#startDisputForm').submit(function(e) {	
+		$(this).find('input[type=submit]').prop('disabled', true);
+		e.preventDefault();
+		transaction.submitDispute(
+			$('#login').val(),
+			$('#password').val(),
+			function(err, response) {
+				location.reload();
+			}
+		);
+		return false;
+	});
+
+	$('#escrowForm').submit(function() {	
+		$(this).find('input[type=submit]').prop('disabled', true);
+		transaction.submitEscrow(
+			$('#login').val(),
+			$('#password').val(),
+			($('#escrowFrom').is(':checked') ? 'from' : 'to'),
+			function(err, response) {
+				location.reload();
+			}
+		);
+		return false;
+	});
+				
+	if(localStorage.getItem('escrow_login') !== undefined && localStorage.getItem('escrow_login')) {
+		$('#sendLogin').val(localStorage.getItem('escrow_login'));
+	}
+		
+	var id = gup('id');		
+	if(id) {
+		transaction = new Transaction(gup('id'), function(transaction) {
+			$('div.' + transaction.status).show();
+			if(!transaction.to_approved) {
+				$('div.waitingForApprovalTo').show();
+			}
+			if(!transaction.agent_approved) {
+				$('div.waitingForApprovalAgent').show();
+			}
+			$('span.transactionId').html('<a href="?id=' + transaction.from + '-' + transaction.escrow_id + '">' + transaction.from + '-' + transaction.escrow_id + '</a>');
+			$('span.transactionFrom').html('<a target="_blank" href="https://golos.io/@' + transaction.from + '">@' + transaction.from + '</a>');
+			$('span.transactionTo').html('<a target="_blank" href="https://golos.io/@' + transaction.to + '">@' + transaction.to + '</a>');
+			$('span.transactionAgent').html('<a target="_blank" href="https://golos.io/@' + transaction.agent + '">@' + transaction.agent + '</a>');
+			$('span.transactionMoney').html(transaction.money);
+			$('span.transactionFee').html(transaction.pending_fee);
+			$('span.transactionDate').html(transaction.escrow_expiration);
+		});			
+		if(transaction.from === undefined) {
+			id = null;
+			$('#tabSend').click();
+		} else {
+			$('#tabCP').click();
+		}
+	} else {
+		$('#tabSend').click();
+		if($('#sendLogin').val()) {
+			$('#sendPassword').focus();
+		} else {
+			$('#sendLogin').focus();
+		}
+	}
+	
+});
